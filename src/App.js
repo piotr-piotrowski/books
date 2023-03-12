d1ba851cf3a6e28e4257e2d7e7f23704ed8b3f2b
Piotr Piotrowski <piotr.j.piotrowski@gmail.com>
2023-03-12 11:21:27 +0100

    feat: update and delete book (also some cleanup)

--- src/App.js
+++ src/App.js
@@ -11,32 +11,33 @@ function App() {
   }, []);
 
   const fetchBooks = async () => {
-    const response = await axios.get('http://localhost:3001/books');
+    const response = await axios.get("http://localhost:3001/books");
     setBooks(response.data);
   };
 
   const createBook = async (title) => {
-    const response = await axios.post('http://localhost:3001/books', {
-      title
+    const response = await axios.post("http://localhost:3001/books", {
+      title,
     });
-    const updatedBooks = [
-     ...books,
-     response.data
-    ];
+    const updatedBooks = [...books, response.data];
     setBooks(updatedBooks);
   };
 
-  const deleteBookById = (id) => {
+  const deleteBookById = async (id) => {
+    await axios.delete(`http://localhost:3001/books/${id}`);
     const updatedBooks = books.filter((book) => {
       return book.id !== id;
     });
     setBooks(updatedBooks);
   };
 
-  const editBookById = (id, newTitle) => {
+  const editBookById = async (id, newTitle) => {
+    const response = await axios.put(`http://localhost:3001/books/${id}`, {
+      title: newTitle,
+    });
     const updatedBooks = books.map((book) => {
       if (book.id === id) {
-        return { ...book, title: newTitle };
+        return { ...book, ...response.data };
       }
       return book;
     });
