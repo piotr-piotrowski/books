1947fe81908010eb42866ad4139ccacc1d4b2d1c
Piotr Piotrowski <piotr.j.piotrowski@gmail.com>
2023-03-12 10:51:09 +0100

    feat: create book (API)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { useState } from "react";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
+import axios from "axios";
 
 function App() {
   const [books, setBooks] = useState([]);
 
-  const createBook = (title) => {
+  const createBook = async (title) => {
+    const response = await axios.post('http://localhost:3001/books', {
+      title
+    });
     const updatedBooks = [
-      ...books,
-      { id: Math.round(Math.random() * 9999), title: title },
+     ...books,
+     response.data
     ];
     setBooks(updatedBooks);
   };
